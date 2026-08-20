c4a816ffdbf9c968d0a3bbf918b93afae214da56
Satish Chandra <imsachandra@gmail.com>
2026-08-20 11:01:28 +0530

    Allow any origin for CORS to fix deployment issue

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174"] : ["http://localhost:5173", "http://localhost:5174"], 
+    origin: (origin, callback) => callback(null, true), 
     credentials: true 
 }));
 app.use(cookieParser());
